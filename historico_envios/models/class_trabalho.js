38490d7411192afd426ea6ae02fcc216dbe5371c
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2021-05-30 21:21:06 -0300

    Added basic style for class_trabalho

---
+++ historico_envios/models/class_trabalho.js
@@ -0,0 +1,9 @@
+class Trabalho {
+    constructor(name, id, delivery_date, grade, student) {
+        this.name = name;
+        this.id = id;
+        this.delivery_date = delivery_date;
+        this.grade = grade;
+        this.student = student;
+    }
+  }
