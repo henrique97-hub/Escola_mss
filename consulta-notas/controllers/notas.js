18c6dd3b81d1c7e81b1647c3ecccd1f812a3a2b4
caio-cruz-garcia <61151058+caio-cruz-garcia@users.noreply.github.com>
2021-06-25 21:12:33 -0300

    correcao de erros

--- consulta-notas/controllers/notas.js
+++
@@ -1,9 +0,0 @@
-const Aluno = require('../models/ver-notas-aluno')
-
-module.exports = app  => {
-    app.get('/ver-notas-aluno/:id', (req, res) => {
-        const id = parseInt(req.params.id)
-
-        Aluno.buscaPorId(id, res)
-    })
-}
